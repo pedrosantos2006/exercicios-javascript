db645a7b3fe78aa94e779c8a4f6d0cbda8e46a1c
Pedro Santos <112526196+pedrosantos2006@users.noreply.github.com>
2022-11-29 20:26:38 -0300

    Atividade concluida

---
+++ ex007/app.js
@@ -0,0 +1,5 @@
+function MostrarNome(){
+    let n1 = document.getElementById("nome").value
+    let n2 = document.getElementById("sobrenome").value
+    document.getElementById("mensagem").innerHTML = "Ola" + n1 +n2
+}
